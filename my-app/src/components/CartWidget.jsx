a23a7678bf5960a08b55ee0429cbfbcc041c4bce
Gonzalo <gonzalo.d.rolando@gmail.com>
2023-09-06 18:16:18 -0300

    prueba

---
+++ my-app/src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+const CartWidget = () => {
+  const itemCount = 3;
+
+  return (
+    <div className="cart-widget">
+      <i className="fas fa-shopping-cart"></i>
+      <span className="badge badge-pill badge-danger">{itemCount}</span>
+    </div>
+  );
+};
+
+export default CartWidget;
