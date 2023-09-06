e3068fa3f0bbfcb9f13285a34bfc788049f43a2d
Gonzalo <gonzalo.d.rolando@gmail.com>
2023-09-06 18:35:02 -0300

    prueba5

---
+++ my-app/src/components/ItemListContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ItemListContainer = ({ greeting }) => {
+    return (
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title text-center">Mensaje de Saludo</h2>
+                <p className="card-text text-center">{greeting}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ItemListContainer
